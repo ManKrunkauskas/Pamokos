babeb62fc45eb9ad0c723008949f800e928e1b34
ManKrunkauskas <kmangirdask@gmail.com>
2024-10-22 13:00:51 +0300

    21uzd

---
+++ 2024-10-21/21uzd/script.js
@@ -0,0 +1,10 @@
+const menuIcon = document.getElementById('menu-icon');
+const navLinks = document.getElementById('nav-links');
+
+menuIcon.addEventListener('click', () => {
+    if (navLinks.style.display === 'flex') {
+        navLinks.style.display = 'none';
+    } else {
+        navLinks.style.display = 'flex';
+    }
+});
