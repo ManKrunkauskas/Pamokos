a47e4d5a4abb2ad6bf39ac2aa32e7c47e788f423
ManKrunkauskas <kmangirdask@gmail.com>
2024-11-06 12:40:20 +0200

    Pridėjau Rectangle užduotį

---
+++ 2024-11-06/30 Užduotis/Rectangle/script.js
@@ -0,0 +1,6 @@
+function calcRectangleArea(length, width) {
+  return length * width;
+}
+
+// Pavyzdys: patikrinkime funkciją su pavyzdiniais duomenimis
+console.log(calcRectangleArea(7, 6)); // Turėtų grąžinti 30
