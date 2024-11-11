6be20500f53ce97cae0d4da9e08edf2bf744ea80
ManKrunkauskas <kmangirdask@gmail.com>
2024-11-11 14:01:07 +0200

    Pridėjau 4. užduotį

---
+++ 2024-11-11/Atsiskaitymas/JavaScript/4./script.js
@@ -0,0 +1,11 @@
+function filterItems(arr, query) {
+  return arr
+    .filter((item) => item.toLowerCase().includes(query.toLowerCase()))
+    .sort()
+    .map((item) => `*${item}`);
+}
+
+const friends = ["Rika", "Jacob", "Alex", "Oliver", "Marika"];
+
+console.log(filterItems(friends, "ka"));
+console.log(filterItems(friends, "e"));
