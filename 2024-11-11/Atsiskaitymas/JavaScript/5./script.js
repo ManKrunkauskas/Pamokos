bca8d8783f33c1295a83c0113c0b09223ef955cc
ManKrunkauskas <kmangirdask@gmail.com>
2024-11-11 14:07:07 +0200

    Pridėjau  5 užduotį

---
+++ 2024-11-11/Atsiskaitymas/JavaScript/5./script.js
@@ -0,0 +1,17 @@
+let P = 10000;
+let R = 8.5;
+let T = 3;
+
+let simpleInterest = (P * R * T) / 100;
+
+console.log(
+  "Jei investuosite " +
+    P +
+    " su " +
+    R +
+    "% palūkanų norma, gausite " +
+    simpleInterest +
+    " per " +
+    T +
+    " metus."
+);
