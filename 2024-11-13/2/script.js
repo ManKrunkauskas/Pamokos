cd5b96c251971525d3a7a345c573a52dd66cc8df
ManKrunkauskas <kmangirdask@gmail.com>
2024-11-14 03:00:28 +0200

    Pridėta 2 užduotis

---
+++ 2024-11-13/2/script.js
@@ -0,0 +1,26 @@
+function calculate(num1, num2, operation) {
+  try {
+    switch (operation) {
+      case "+":
+        return num1 + num2;
+      case "-":
+        return num1 - num2;
+      case "*":
+        return num1 * num2;
+      case "/":
+        if (num2 === 0) {
+          throw new Error("Cannot divide by zero");
+        }
+        return num1 / num2;
+      default:
+        throw new Error("Invalid operation");
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+console.log(calculate(10, 2, "+"));
+console.log(calculate(10, 2, "/"));
+console.log(calculate(10, 2, "/"));
+console.log(calculate(10, 13, "&"));
