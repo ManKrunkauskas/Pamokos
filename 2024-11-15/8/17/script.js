10e31cf82c37a57c863e83a7df03e3e0ce2333f9
ManKrunkauskas <kmangirdask@gmail.com>
2024-11-15 13:00:44 +0200

    Add

---
+++ 2024-11-15/8/17/script.js
@@ -0,0 +1,9 @@
+function Unix_timestamp(timestamp) {
+  const date = new Date(timestamp * 1000);
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  const seconds = date.getSeconds().toString().padStart(2, "0");
+  return `${hours}:${minutes}:${seconds}`;
+}
+
+console.log(Unix_timestamp(1412743274));
